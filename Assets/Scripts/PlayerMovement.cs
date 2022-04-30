36e19466476c8098b4b5f674bb213855cecc03af
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-04-30 22:28:17 +0530

    Player movement and Animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float playerSpeed;
+    public float rotationSpeed;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement =new Vector3(Input.GetAxis("Horizontal") * playerSpeed,0f, Input.GetAxis("Vertical") * playerSpeed);
+       
+        transform.Translate(movement.x*Time.deltaTime, 0f, movement.z*Time.deltaTime);
+        if (movement.magnitude != 0)
+            animator.SetFloat("Blend",movement.magnitude);
+        float rotateX = Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.Rotate(0f, rotateX, 0f);
+
+    }
+
+}
