31d3b3d279ae8031b1b5d9963b5065762a14da7e
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-05-01 21:25:25 +0530

    Audio and Animation Events added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,20 +8,25 @@ public class PlayerMovement : MonoBehaviour
     public float playerSpeed;
     public float rotationSpeed;
     Animator animator;
-
+    AudioSource audioSource;
+    public AudioClip shootClip;
+    public AudioClip walkClip;
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement =new Vector3(Input.GetAxis("Horizontal") * playerSpeed,0f, Input.GetAxis("Vertical") * playerSpeed);
+        Vector3 movement =new Vector3(Input.GetAxis("Horizontal"),0f, Input.GetAxis("Vertical"));
        
-        transform.Translate(movement.x*Time.deltaTime, 0f, movement.z*Time.deltaTime);
-        if (movement.x!=0 || movement.z!=0)
-            animator.SetFloat("Blend",movement.magnitude);
+        transform.Translate(movement.x*Time.deltaTime* playerSpeed, 0f, movement.z*Time.deltaTime * playerSpeed);
+        if (movement.x != 0 || movement.z != 0)
+            animator.SetFloat("Blend", movement.magnitude);
+        else
+            animator.SetFloat("Blend", 0f);
         float rotateX = Input.GetAxis("Mouse X") * rotationSpeed;
         transform.Rotate(0f, rotateX, 0f);
         if(Input.GetKeyDown(KeyCode.Space))
@@ -29,6 +34,16 @@ public class PlayerMovement : MonoBehaviour
             animator.SetTrigger("IsAttack");
         }
 
+    }
+    public void ShotFire()
+    {
+        audioSource.clip = shootClip;
+        audioSource.Play();
+    }
+    public void Walk()
+    {
+        audioSource.clip = walkClip;
+        audioSource.Play();
     }
 
 }
